5b6ffa90dff25ea4c96e0b4e41fa3225b2ca4638
pdaambrosio <pda.ambrosio@gmail.com>
2023-04-06 08:58:41 -0300

    feat: class rockPaperScissor

--- src/rockPaperScissor.js
+++ src/rockPaperScissor.js
@@ -1,1 +1,9 @@
 const inquirer = require("inquirer");
+
+module.exports = class rockPaperScissor = () => {
+    constructor() {
+        this.prompt = inquirer.createPromptModule();
+        this.choices = ["Rock", "Paper", "Scissor"];
+    }
+};
+
