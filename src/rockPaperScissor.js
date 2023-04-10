4063a0a5b959dfc15185f85e5a57a421b1f02ec2
pdaambrosio <pda.ambrosio@gmail.com>
2023-04-10 08:57:33 -0300

    feat: choose options

--- src/rockPaperScissor.js
+++ src/rockPaperScissor.js
@@ -18,4 +18,17 @@ module.exports = class rockPaperScissor {
     }
     this.chooseGameOption();
   }
+
+  async chooseGameOption() {
+    let answer = await this.prompt({
+      type: "list",
+      name: "gameOption",
+      message: "Choose an option",
+      choices: [
+        { name: "Player vs Computer", value: "PlayerVsComputer" },
+        { name: "Computer vs Computer", value: "computerVsComputer" },
+      ],
+    });
+    this.compete(answer.gameOption);
+  }
 };
