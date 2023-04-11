2e3e630ba01840fcb3430b6204dbdbd536719c84
pdaambrosio <pda.ambrosio@gmail.com>
2023-04-11 07:58:34 -0300

    feat: game logical compete

--- src/rockPaperScissor.js
+++ src/rockPaperScissor.js
@@ -31,4 +31,33 @@ module.exports = class rockPaperScissor {
     });
     this.compete(answer.gameOption);
   }
+
+  async compete(gameOption) {
+    let player1Label = "";
+    let player1Move = "";
+    let result = "";
+    let computerMove = this.randomMove();
+    if (gameOption === "PlayerVsComputer") {
+      player1Move = await this.getUserMove();
+      player1Label = "You";
+    } else if (gameOption === "computerVsComputer") {
+      player1Move = this.randomMove();
+      player1Label = "Computer 1";
+    }
+
+    if (player1Move === "") {
+      console.log("Something went wrong. Please try again");
+      process.exit(0);
+    }
+
+    let winner = this.getWinner(player1Move, computerMove, player1Label);
+
+    this.printResult(player1Label, player1Move, computerMove, winner);
+    this.start();
+  }
+
+  getWinner(player1Move, computerMove, player1Label) {
+    let winner = "";
+  #TODO: Add logic to determine the winner
+  }
 };
